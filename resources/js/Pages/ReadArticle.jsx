22ff9d0fd277ccd78150471f6539fa98c2233027
Jessel Zapanta <jeselzapanta9@gmail.com>
2024-08-23 21:51:28 +0800

    test

--- resources/js/Pages/ReadArticle.jsx
+++ resources/js/Pages/ReadArticle.jsx
@@ -224,6 +224,7 @@ export default function ReadArticle({
                                 <p className="text-gray-400 w-full text-center">
                                     Please log in to your account to comment.
                                 </p>
+                                <p>daklsjd;asjdjlak</p>
                             </Link>
                         )}
                     </div>
