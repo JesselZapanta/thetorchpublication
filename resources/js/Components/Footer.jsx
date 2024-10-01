7c2e010efc7979b42c033a1e8d276f85ecbe44cd
JesselZapanta <jesselzapanta9@gmail.com>
2024-10-01 11:51:55 +0800

    edit the footer

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from '@inertiajs/react';
 import React from 'react'
 
 export default function Footer() {
@@ -7,36 +8,56 @@ export default function Footer() {
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="my-4 bg-white h-[1px] w-full"></div>
 
-                    <div className="flex flex-col justify-around gap-6 lg:flex-row">
-                        <div className="flex-1">
+                    <div className="grid sm:grid-cols-1 lg:grid-cols-3 gap-4">
+                        <div>
                             <h1 className="font-bold text-white mb-4">TCGC</h1>
                             <ul className="text-white">
                                 <li>Tangub City, Misamis Occidental</li>
                                 <li>(088)-545-2793</li>
-                                <li>gadtc@gadtc.edu.ph</li>
-                                <li>www.facebook.com/gadtcluxmundi</li>
+                                <li>
+                                    <a href="gadtc@gadtc.edu.ph" target="blank">
+                                        gadtc@gadtc.edu.ph
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://www.facebook.com/tcgcluxmundi"
+                                        target="blank"
+                                    >
+                                        facebook.com/tcgcluxmundi
+                                    </a>
+                                </li>
                             </ul>
                         </div>
-                        <div className="flex-1">
+                        <div>
                             <h2 className="font-bold text-white mb-4">
                                 The Torch Publication
                             </h2>
                             <p className="text-white">
-                                The Torch is Tangub City Global College's
-                                official publication. "Lorem ipsum dolor sit
-                                amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore
-                                magna aliqua.
+                                    The Torch is Tangub City Global College's
+                                    official publication. 
                             </p>
                         </div>
 
-                        <div className="flex-1">
+                        <div>
                             <div className="text-white font-bold mb-4">
-                                <a href="">The Torch</a>
+                                <a href="">The Torch Socials</a>
                             </div>
-                            <div className="text-white">
-                                <a href="">torch@gmail.com</a>
-                            </div>
+                            <ul className="text-white">
+                                <li>
+                                    <a href="torch@gmail.com" target="blank">
+                                        torch@gmail.com
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://www.facebook.com/TheTORCHofficialpage"
+                                        target="blank"
+                                    >
+                                        facebook.com/TheTORCHofficialpage
+                                    </a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
