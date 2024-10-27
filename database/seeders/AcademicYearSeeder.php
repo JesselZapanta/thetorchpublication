08675cb1ca18b4d573ba5699267c3460738c3637
JesselZapanta <jesselzapanta9@gmail.com>
2024-10-27 13:31:12 +0800

    update the academic year seeder

--- database/seeders/AcademicYearSeeder.php
+++ database/seeders/AcademicYearSeeder.php
@@ -13,28 +13,6 @@ class AcademicYearSeeder extends Seeder
     public function run(): void
     {
         $data = [
-            
-            [
-                'code' => '241',
-                'description' => '1ST SEM AY 2024-2025',
-                'status' => 'active',
-                'start_at' => '2024-08-01 00:00:00',
-                'end_at' => '2024-12-27 00:00:00',
-            ],
-            [
-                'code' => '242',
-                'description' => '2ND SEM AY 2024-2025',
-                'status' => 'inactive',
-                'start_at' => '2025-01-01 00:00:00',
-                'end_at' => '2025-06-01 00:00:00',
-            ],
-            [
-                'code' => '243',
-                'description' => 'SUMMER AY 2024-2025',
-                'status' => 'inactive',
-                'start_at' => '2024-06-01 00:00:00',
-                'end_at' => '2024-08-01 00:00:00',
-            ],
             [
                 'code' => '231',
                 'description' => '1ST SEM AY 2023-2024',
@@ -56,15 +34,27 @@ class AcademicYearSeeder extends Seeder
                 'start_at' => '2024-05-31 00:00:00',
                 'end_at' => '2024-07-31 00:00:00',
             ],
-
             [
                 'code' => '241',
                 'description' => '1ST SEM AY 2024-2025',
-                'status' => 'inactive',
+                'status' => 'active',
                 'start_at' => '2024-08-01 00:00:00',
                 'end_at' => '2024-12-27 00:00:00',
-            ]
-            
+            ],
+            // [
+            //     'code' => '242',
+            //     'description' => '2ND SEM AY 2024-2025',
+            //     'status' => 'inactive',
+            //     'start_at' => '2025-01-01 00:00:00',
+            //     'end_at' => '2025-06-01 00:00:00',
+            // ],
+            // [
+            //     'code' => '243',
+            //     'description' => 'SUMMER AY 2024-2025',
+            //     'status' => 'inactive',
+            //     'start_at' => '2024-06-01 00:00:00',
+            //     'end_at' => '2024-08-01 00:00:00',
+            // ],
         ];
         \App\Models\AcademicYear::insertOrIgnore($data);
     }
