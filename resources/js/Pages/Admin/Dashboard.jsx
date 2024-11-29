2332c24f18527dda713b4e8f1207fbc53d2ea12f
JesselZapanta <jesselzapanta9@gmail.com>
2024-11-29 15:48:49 +0800

    remove

--- resources/js/Pages/Admin/Dashboard.jsx
+++ resources/js/Pages/Admin/Dashboard.jsx
@@ -13,7 +13,6 @@ export default function Dashboard({
 
     categoriesWithCount,
     categoriesWithViewsCount,
-    dateFrom,
 }) {
     const [selectedPeriod, setSelectedPeriod] = useState("daily");
     const [selectedMonth, setSelectedMonth] = useState("");
@@ -81,12 +80,6 @@ export default function Dashboard({
                         Admin Dashboard
                     </h2>
                     <div className="flex gap-4">
-                        {/* <a
-                            href="/backup/download"
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                        >
-                            Download SQL Backup
-                        </a> */}
                         <Link
                             href={route("admin.report")}
                             className="px-4 py-2 bg-indigo-600 text-nowrap text-gray-50 transition-all duration-300 rounded hover:bg-indigo-700"
